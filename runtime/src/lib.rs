0486563deb83ac24d3bc8a77e7767815b2bef153
Akshay <araa@connect.ust.hk>
2020-05-06 14:55:07 +0800

    remove storage from multi_transfer initalization in contruct_runtime

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -249,7 +249,7 @@ construct_runtime!(
 		Sudo: sudo::{Module, Call, Config<T>, Storage, Event<T>},
 		// Used for the module template in `./template.rs`
 		TemplateModule: template::{Module, Call, Storage, Event<T>},
-		MultiTransfer: multi_transfer::{Module, Call, Storage, Event<T>},
+		MultiTransfer: multi_transfer::{Module, Call, Event<T>},
 	}
 );
 
