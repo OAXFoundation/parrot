51d70a35a69ac3dd7ac1d02a9cb9ce01f7edf9a1
Akshay <araa@connect.ust.hk>
2020-06-11 14:53:06 +0800

    increased existential deposit

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -258,7 +258,7 @@ impl timestamp::Trait for Runtime {
 }
 
 parameter_types! {
-	pub const ExistentialDeposit: u128 = 500;
+	pub const ExistentialDeposit: u128 = 1 * DOLLARS;
 }
 
 impl balances::Trait for Runtime {
